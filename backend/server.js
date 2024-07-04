c6ed5cd3f83d1c3369ef9c3b1d82ffb40c8ef07a
RajGupta15082004 <graj25776@gmail.com>
2024-07-04 22:38:03 +0530

    3-payload limit set

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,48 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
-import {v2 as cloudinary} from "cloudinary";
 import notificationRoutes from "./routes/notification.route.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CCLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const app=express();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-const PORT=process.env.PORT || 5000;
-
-// console.log(process.env.MONGO_URI);
-
-app.use(express.json());//middleware=a regular func that runs betw request and response= we are using it to parse the req.body here  // Middleware for JSON: Adds a middleware to parse incoming JSON requests. It runs between the request and response phases, enabling req.body to contain the parsed JSON.
-
-app.use(express.urlencoded({extended:true}));//to parse form data(urlencoded)
-// Middleware for URL-encoded Data: Adds a middleware to parse URL-encoded data (e.g., form submissions). The extended: true option allows for rich objects and arrays to be encoded. The extended: true option allows for rich objects and arrays to be encoded. Rich objects refer to complex data structures that include arrays and nested objects
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
-//Cookie Parser Middleware: Adds a middleware to parse cookies attached to the client request object. It makes req.cookies available for accessing cookies.
 
-//Add all routes here
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/posts",postRoutes);
-app.use("/api/notifications",notificationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
-app.listen(PORT,()=>{
-  console.log(`server is running on port ${PORT}`);
-  connectMongoDB();
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
 });
